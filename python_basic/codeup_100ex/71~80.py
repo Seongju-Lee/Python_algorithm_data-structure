6c16504e9581a692ae4ab269626e7e655399c6a7
이성주 <67941526+Seongju-Lee@users.noreply.github.com>
2021-01-16 23:16:34 +0900

    Update 71~80.py

--- python_basic/codeup_100ex/71~80.py
+++ python_basic/codeup_100ex/71~80.py
@@ -35,9 +35,63 @@
 
 # 75번 문제
 
-a = int(input())
+# a = int(input())
 
-while a != 0:
-    a -= 1
-    print(a)
+# while a != 0:
+#     a -= 1
+#     print(a)
+
+
+# 76번 문제
+
+# print(ord('a'))
+# a = input()
+
+# ascii_num = ord(a)
+# tmp = ascii_num - 97
+
+# for i in range(tmp + 1):
+#     print(chr(97 + i), end=' ')
+
+
+# 77번 문제
+
+# a = int(input())
+
+# for i in range(a + 1):
+#     print(i)
+
+
+# 78번 문제
+
+# a = int(input())
+# temp = 0
+# for i in range(a + 1):
+#     if i % 2 == 0:
+#         temp += i
     
+# print(temp)
+
+
+
+# 79번 문제
+
+# a = list(input().split())
+
+# for i in range(len(a)):
+#     print(a[i])
+#     if a[i] == 'q':
+#         break
+
+
+# 80번 문제
+
+a = int(input())
+tmp = 0
+s = 0
+
+while tmp < a:
+    s += 1
+    tmp += s
+
+print(s)
